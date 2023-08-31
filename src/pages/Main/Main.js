8e2e7d359b1a2ef7a73fcb287c95d1ff32dae30d
HojuShin <shin03188@naver.com>
2023-08-31 22:42:38 +0900

    style: 메인페이지 이미지 자동 슬라이드 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,51 @@
 import './main.css';
+import Header from '../../components/Header/Header.js';
+import movie from '../../data/movie.json';
 
 function Main() {
     return (
         <>
-            <p>테스트</p>
+            <div className='container'>
+                <Header></Header>
+                <main>
+                    <div className='backgroundSection'>
+                        <input type='radio' className='slide' id='slide01' defaultChecked></input>
+                        <input type='radio' className='slide' id='slide02'></input>
+                        <input type='radio' className='slide' id='slide03'></input>
+                        <input type='radio' className='slide' id='slide04'></input>
+                        <ul className='slideList'>
+                            <li className='slideItem'>
+                                <div>
+                                    <label htmlFor='slide04'></label>
+                                    <label htmlFor='slide02'></label>
+                                    <a><img src={movie[0].detailImg} alt={movie[0].title}></img> </a>
+                                </div>
+                            </li>
+                            <li className='slideItem'>
+                                <div>
+                                    <label htmlFor='slide01'></label>
+                                    <label htmlFor='slide03'></label>
+                                    <a><img src={movie[1].detailImg} alt={movie[1].title}></img> </a>
+                                </div>
+                            </li>
+                            <li className='slideItem'>
+                                <div>
+                                    <label htmlFor='slide02'></label>
+                                    <label htmlFor='slide04'></label>
+                                    <a><img src={movie[2].detailImg} alt={movie[2].title}></img> </a>
+                                </div>
+                            </li>
+                            <li className='slideItem'>
+                                <div>
+                                    <label htmlFor='slide03'></label>
+                                    <label htmlFor='slide01'></label>
+                                    <a><img src={movie[3].detailImg} alt={movie[3].title}></img> </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </main>
+            </div>
         </>
     )
 }
