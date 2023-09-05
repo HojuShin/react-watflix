82152bb63a2e15aaf5246ce2b725b15e9330017a
HojuShin <shin03188@naver.com>
2023-09-05 22:32:18 +0900

    feat: Redux 데이터 저장 및 버튼스타일 변경을 위한 버튼 클릭 로직 구현

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -2,13 +2,39 @@ import { useParams } from 'react-router-dom';
 import './detail.css';
 import Header from '../../components/Header/Header.js';
 import allMovie from '../../data/allMovie.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addLocker, deleteLocker } from '../../redux/store';
 
 function Detail() {
     //현재 라우팅된 페이지의 url 파라미터 추출 
     const { id } = useParams();
 
     //현재 페이지 id 파라미터와 일치하는 영화 데이터 가져와서 matchId에 저장 
-    const matchId = allMovie.find((movie) => { return movie.id == id })
+    const matchId = allMovie.find((movie) => { return movie.id == id });
+
+    // redux store 가져오기 
+    const state = useSelector((state) => state.locker);
+    const dispatch = useDispatch();
+    console.log('리덕스', state);
+
+    // 보고싶어요 버튼 클릭 상태
+    const [onclick, setOnClick] = useState(false);
+
+    // 보고싶어요 버튼 클릭 동작 
+    const handleClick = () => {
+        setOnClick(!onclick);
+
+        // store에 이미 추가되었다면, 데이터를 store에서 삭제
+        // 그렇지 않으면, 데이터를 store에 추가
+        if (onclick) {
+            return dispatch(deleteLocker(matchId))
+        } else if (!onclick === true) {
+            return dispatch(addLocker(matchId))
+        }
+    };
 
     return (
         <>
@@ -29,8 +55,18 @@ function Detail() {
                         <p className='movieStory'>{matchId.story}</p>
                         <div className="mvPlay">
                             <button className="playBtn">▶  PLAY NOW</button>
-                            <button id="addLocker">
-                                보고싶어요
+                            <button id="addLocker" onClick={handleClick}>
+                                {onclick ? (
+                                    <>
+                                        <FontAwesomeIcon icon={faCheck} style={{ margin: '0px 10px 0px 0px', color: "rgb(222, 42, 96)" }} />
+                                        보고싶어요
+                                    </>
+                                ) : (
+                                    <>
+                                        <FontAwesomeIcon icon={faPlus} style={{ margin: '0px 10px 0px 0px', color: "#eee" }} />
+                                        보고싶어요
+                                    </>
+                                )}
                             </button>
                         </div>
                     </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,35 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 //reducer 정의
-const store = createSlice({
-  name: 'store',
+const locker = createSlice({
+  name: 'locker',
   initialState: [], //초기 상태
   reducers: {
+    // locker에 추가 동작
+    addLocker(state, action) {
+      state.push(action.payload);
+    },
+    // locker 에서 제거 동작
+    deleteLocker(state, action) {
+      // locker 에서 해당 title을 가진 객체 삭제 
+      const lockerTitle = state.find(e => e.title === action.payload.title);
+      for (var i = 0; i < state.length; i++) {
+        if (state[i].title == lockerTitle.title) {
+          state.splice(i, 1);
+          // 배열 길이 변화
+          i--;
+        }
+      }
+    },
   }
 })
+
+// action 내보내기
+export const { addLocker, deleteLocker } = locker.actions;
 
 //store 구성
 export default configureStore({
   reducer: {
+    locker: locker.reducer
   }
 })
