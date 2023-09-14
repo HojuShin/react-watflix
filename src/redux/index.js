5a0acdfc6b0a38d776f83da184f58bd712d4f65d
HojuShin <shin03188@naver.com>
2023-09-14 21:11:08 +0900

    Fix: redux 툴킷 직렬화 에러 해결

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,7 @@
 import storage from 'redux-persist/lib/storage'; // local storage 저장
 import locker from './store';
 import { persistStore, persistReducer } from 'redux-persist'
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 const persistConfig = {
     key: "root",
@@ -17,7 +17,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //store 설정 (persistedReducer로 상태 관리)
 const store = configureStore({
-    reducer: persistedReducer
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+      }),
 });
 
 export const persistor = persistStore(store);
